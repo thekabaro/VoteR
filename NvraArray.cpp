96f19fb104663af5a3cb3b8d86bdc9a3bdc0e181
Michael Jarriel <thekabaro@gmail.com>
2018-09-19 12:45:39 -0500

    Create NvraArray.cpp

---
+++ NvraArray.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+#include "NvraArray.h"
+
+// constructs array with default capacity
+NvraArray::NvraArray() {
+	// FIXME
+}
+
+// constructs array with specified capacity
+NvraArray::NvraArray(int capacity) {
+	// FIXME
+}
+
+// frees array space as object is deleted
+NvraArray::~NvraArray() {
+	// FIXME
+}
+
+// adds record, increments size, doubles capacity as necessary
+void NvraArray::add(NvraRecord* record) {
+	// FIXME
+}
+
+// returns (copy of) record at index
+NvraRecord NvraArray::get(int index) {
+	// FIXME
+}
