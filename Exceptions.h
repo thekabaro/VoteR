33a99f7d41587f1de830a8e5aae5afb43f7bb663
Michael Jarriel <thekabaro@gmail.com>
2018-11-04 12:02:46 -0600

    Update Exceptions.h

--- Exceptions.h
+++ Exceptions.h
@@ -1,6 +1,7 @@
 #ifndef EXCEPTIONS_H
 #define EXCEPTIONS_H
 
+// exceptions to throw
 class Exception {};
 class ExceptionIndexOutOfRange : Exception {};
 class ExceptionMemoryNotAvailable : Exception {};
