1f8bf638ac5ea24edd558ad4507c8c2632ce2c55
Michael Jarriel <thekabaro@gmail.com>
2018-11-04 12:02:29 -0600

    Update Sorter.h

--- Sorter.h
+++ Sorter.h
@@ -8,29 +8,36 @@
 template <typename T>
 class Sorter {
 private:
+	// templated swap function
 	static void swap(TemplatedArray<T>& array, long long i, long long j);
+	// templated quick sort (using the psuedocode from the exam)
 	static void quickSort(TemplatedArray<T>& array, long long left, long long right, const Comparator<T>& comparator);
 public:
+	// the initial starting point of sorter.h
 	static void sort(TemplatedArray<T>& array, const Comparator<T>& comparator);
 };
 
+// templated swap function
 template <typename T>
 void Sorter<T>::swap(TemplatedArray<T>& array, long long i, long long j) {
 	T temp = array[i];
-	array.replaceAt(&array[j], i);
+	T arrayJ = array[j];
+	array.replaceAt(&arrayJ, i);
 	array.replaceAt(&temp, j);
 }
 
+// templated quick sort (using the psuedocode from the exam)
 template <typename T>
 void Sorter<T>::quickSort(TemplatedArray<T>& array, long long left, long long right, const Comparator<T>& comparator) {
 	if (left < right) {
-		long long int pivot = (left + right) / 2;
-		long long int i = left - 1;
+		long long sizeOfArray = array.getSize();
+		long long pivot = (left + right) / 2;
+		long long i = left - 1;
 		long long j = right + 1;
 		do {
 			do {
 				i++;
-			} while ((i < array.getSize()) && (comparator.compare(array[i], array[pivot]) <= 0));
+			} while ((i < sizeOfArray) && (comparator.compare(array[i], array[pivot]) <= 0));
 			do {
 				j--;
 			} while ((j >= i) && (comparator.compare(array[j], array[pivot]) >= 0));
@@ -47,11 +54,15 @@ void Sorter<T>::quickSort(TemplatedArray<T>& array, long long left, long long ri
 	}
 }
 
+// the initial starting point of sorter.h
 template <typename T>
 void Sorter<T>::sort(TemplatedArray<T>& array, const Comparator<T>& comparator) {
+	// already sorted (only 1 element)
 	if (array.getSize() == 0) {
 		return;
 	}
+
+	// start the sort
 	quickSort(array, 0, array.getSize() - 1, comparator);
 }
 
